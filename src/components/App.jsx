e70eeee5c67bd623792abe6571173542eeab2711
Olena Iankovska <121794911+OlenaIa@users.noreply.github.com>
2023-08-02 10:57:48 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { MainTitle } from "./App.styled";
+import { Feedback } from "./Feedback/Feedback";
+
+export const App = () => {
+  return (
+    <>
+      <MainTitle>Feedback</MainTitle>
+      <Feedback />
+    </>
+  );
+};
